c811dd29ddcc04af6e7915132d4f60a2d96feb79
andredly <emubem1@gmail.com>
2019-11-04 20:54:26 +0300

    Task 1 Core concepts lecture. Refactor

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,11 @@
 const HtmlWebPackPlugin = require("html-webpack-plugin");
+const path = require('path');
 module.exports = {
+    entry: "./src/index.js",
+    output: {
+        path: path.join(__dirname, 'dist'),
+        filename: "[name].js"
+    },
     module: {
         rules: [
             {
